3a2e26f7f077b6743c53af0bf1d1b2de7e3981ca
StefanFaustian <stefanpetcovici@gmail.com>
2025-12-28 20:56:06 +0200

    Detectarea optiunilor duplicate si suprascrise

--- sshcheck.sh
+++ sshcheck.sh
@@ -28,19 +28,51 @@ OPTIUNI=(
 	"AllowTcpForwarding=no"
 	"X11Forwarding=no"
 	"PermitTunnel=no"
-
+	"MaxAuthTries=4"
+	"PermitUserEnvironment=no"
+	"LoginGraceTime=30"
 )	
 
 for optiune in "${OPTIUNI[@]}"; do
+	echo
 	key="${optiune%%=*}"  ## elimina cel mai lung sufix
 	recomandare="${optiune#*=}"   ## elimina cel mai lung prefix
-	valoare=$(grep "^[[:space:]]*$key" "$fisier" | awk '{print $2;}')
+	valoare=$(grep -E "^[[:space:]]*$key[[:space:]]+" "$fisier" | tail -n 1 | awk '{print $2}')
+	potriviri=$(grep -cE "^[[:space:]]*$key[[:space:]]+" "$fisier")                                                         
 	
+	if (( potriviri >= 2 )); then
+		awk -v key="$key" '
+		$0 ~ /^[[:space:]]*#/ {next}  # omite liniile comentate
+		$1 == key {
+			if (!sw) {
+				sw=1
+				prima_ap=NR
+				ultima_ap=NR
+				ultima_val=$2
+				next
+			}
+			
+			
+			if ($2 != ultima_val) {
+				print "SUPRASCRIERE: \"" key " " $2 "\" la linia " NR " (conflict cu \"" key " " ultima_val "\")."
+			}
+			else {
+				print "DUPLICAT: \"" key " " $2 "\" se repeta la linia " NR " (aparitie initiala la linia " prima_ap ")."
+			}
+			
+			ultima_ap=NR
+			ultima_val=$2
+		}
+		END {
+			if (sw) print "IN EFECT: (" ultima_ap ") " key " " ultima_val 
+		}
+		
+		' "$fisier"
+	fi
 	
-	
-	if [[ -z "$valoare" ]]; then
-		if grep -qiE "^[[:space:]]*#[[:space:]]*$key([[:space:]]+|$)" "$fisier"; then
-      			echo "ATENTIE: optiunea $key este comentata (implicita/nesetata explicit)."
+	if [[ -z "$valoare" ]]; then                                               
+		if grep -qE "^[[:space:]]*#[[:space:]]*$key[[:space:]]+" "$fisier"; then
+      			echo "ATENTIE: optiunea $key este comentata (valoare implicita/nesetata explicit)."
     		else
       			echo "ATENTIE: optiunea $key nu a fost gasita."
     		fi
